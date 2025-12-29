d04cebd4153ac366546b6716bbd795bc12a0dfe8
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2025-12-28 21:18:06 -0300

    Adicionando arquivos para upload

---
+++ controle-producao/src/test/java/com/clinica/controle_producao/ControleProducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.controle_producao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleProducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
